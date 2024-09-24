0d47e5c6374abe49710529ffa625bbd97e3d3ba3
kzorina <kateryna.zorina@cvut.cz>
2024-09-24 13:44:12 +0200

    Preparing change to operate with bop log folders

--- sam/bop_to_table.py
+++ sam/bop_to_table.py
@@ -1,4 +1,7 @@
 import pickle
+from pathlib import Path
+from GlobalParams import GlobalParams
+import numpy as np
 
 def display_table(data_dict, col_width=10):
     row_names = sorted(list(set([t for (t, r) in data_dict.keys()])))
@@ -22,13 +25,47 @@ def display_table(data_dict, col_width=10):
         # Print the entire row
         print(row_str)
 
-recall_file = '/home/ros/kzorina/vojtas/ycbv/ablation_kz_recall.p'
-precision_file = '/home/ros/kzorina/vojtas/ycbv/ablation_kz_precision.p'
+# # Variant 1: load from manually saved file
+# recall_file = '/home/ros/kzorina/vojtas/ycbv/ablation_kz_recall.p'
+# precision_file = '/home/ros/kzorina/vojtas/ycbv/ablation_kz_precision.p'
+#
+# recall_data = pickle.load(open(recall_file, 'rb'))
+# precision_data = pickle.load(open(precision_file, 'rb'))
 
-recall_data = pickle.load(open(recall_file, 'rb'))
-precision_data = pickle.load(open(precision_file, 'rb'))
+# Variant 2: load from bop saved logs
+bop_log_dir = Path('/home/ros/kzorina/vojtas/bop_eval')
+DATASET_NAME = 'ycbv'
+METHOD_BACKBONE = 'cosy_'
+COMMENT = 'synt_real_0.0_threshold_'
+which_modality = 'dynamic'  # 'static', 'dynamic'
 
-print("Recall results")
-display_table(recall_data)
-print("Precision results")
-display_table(precision_data)
+if which_modality == 'static':
+    base_params = GlobalParams(
+                        cov_drift_lin_vel=0.00000001,
+                        cov_drift_ang_vel=0.0000001,
+                        outlier_rejection_treshold_trans = 0.10,
+                        outlier_rejection_treshold_rot = 10*np.pi/180,
+                        t_validity_treshold=0.000025,
+                        R_validity_treshold=0.00075,
+                        max_derivative_order=0,
+                        reject_overlaps=0.05)
+elif which_modality == 'dynamic':
+    base_params = GlobalParams(
+                        cov_drift_lin_vel=0.1,
+                        cov_drift_ang_vel=1,
+                        outlier_rejection_treshold_trans=0.10,
+                        outlier_rejection_treshold_rot=10*np.pi/180,
+                        t_validity_treshold=0.000005,
+                        R_validity_treshold=0.00075,
+                        max_derivative_order=1,
+                        reject_overlaps=0.05)
+
+eval_dir = f'gtsam_{DATASET_NAME}-test_{METHOD_BACKBONE}{COMMENT}{str(base_params)}.csv'
+print(bop_log_dir / eval_dir)
+# /gtsam_ycbv-test_cosy_synt_real_0.0_threshold_1_1_1.0_0.1_1_0.1_0.174532925│   1.0e-03      0.91      0.76      0.68      0.65      0.64
+# 19943295_1.00E-05_1.00E-04/scores_bop19.json
+#
+# print("Recall results")
+# display_table(recall_data)
+# print("Precision results")
+# display_table(precision_data)
