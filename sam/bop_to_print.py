2287cb1d1f5a04f58849ab8075f4dc6d3824b66b
kzorina <kateryna.zorina@cvut.cz>
2024-09-25 10:42:49 +0200

    Fix a bug in the print file

--- sam/bop_to_print.py
+++ sam/bop_to_print.py
@@ -4,7 +4,9 @@ import json
 
 eval_dir = '/home/ros/sandbox_mf/bop_toolkit/data/evals'
 # gtsam_ycbv-test_mega_1_1_1.0_0.1_1_0.1_0.17453292519943295_5.00E-06_9.37E-04
-for dir, _, _ in os.walk(eval_dir):
+root, dirs, files = next(os.walk(eval_dir))
+for dir in dirs:
+    print(dir)
     method, dataset, backbone = dir.split('_')[3:]
     tvt, rvt = dir.split('_')[-2:]
     scores = json.load(open(str(Path(eval_dir) / dir / 'scores_bop24.json')))
