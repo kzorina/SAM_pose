fc23e5b615c31109eedcab76b0b1875b65ad0aa2
kzorina <kateryna.zorina@cvut.cz>
2024-09-25 10:39:34 +0200

    Load reading from json files and print

---
+++ sam/bop_to_print.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+import json
+
+eval_dir = '/home/ros/sandbox_mf/bop_toolkit/data/evals'
+# gtsam_ycbv-test_mega_1_1_1.0_0.1_1_0.1_0.17453292519943295_5.00E-06_9.37E-04
+for dir, _, _ in os.walk(eval_dir):
+    method, dataset, backbone = dir.split('_')[3:]
+    tvt, rvt = dir.split('_')[-2:]
+    scores = json.load(open(str(Path(eval_dir) / dir / 'scores_bop24.json')))
+    # print(f"{method}, {dataset}, {backbone} | {tvt}, {rvt} recall = {}")
+    print(f"{method}, {dataset}, {backbone} | {tvt}, {rvt} precision = {scores['bop24_mAP']}")
