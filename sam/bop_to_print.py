a47955390f467b90982c57662a7fbeb5570f8a88
kzorina <kateryna.zorina@cvut.cz>
2024-09-25 10:43:39 +0200

    Fix a bug in the print file

--- sam/bop_to_print.py
+++ sam/bop_to_print.py
@@ -7,7 +7,7 @@ eval_dir = '/home/ros/sandbox_mf/bop_toolkit/data/evals'
 root, dirs, files = next(os.walk(eval_dir))
 for dir in dirs:
     print(dir)
-    method, dataset, backbone = dir.split('_')[3:]
+    method, dataset, backbone = dir.split('_')[:3]
     tvt, rvt = dir.split('_')[-2:]
     scores = json.load(open(str(Path(eval_dir) / dir / 'scores_bop24.json')))
     # print(f"{method}, {dataset}, {backbone} | {tvt}, {rvt} recall = {}")
